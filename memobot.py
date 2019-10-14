ef30e21ab9a6b91effa930be6affda806edd4449
EPiC_Inc <emmettwb@gmail.com>
2019-10-14 09:43:43 -0600

    Saving what little progress I have done

--- memobot.py
+++ memobot.py
@@ -14,7 +14,9 @@ async def on_ready():
     print("Logged in as " + client.user.name)
 
 @client.command(pass_context=True)
-async def test(context):
+async def test(context): #~test
     await context.send("test")
+async def pull(context): #~pull
+    await context.send("not yet implemented") #TODO: Add stuff from news aggregator i can't spell
 
 client.run(TOKEN)
